3d6fd655e093ad96648ba1877134ddf226aaf8e4
mausneg <maulanasuryanegara123@gmail.com>
2023-06-14 13:08:40 +0800

    transaction

---
+++ app/views/Transaction/transaction.php
@@ -0,0 +1,24 @@
+<div class="container-body">
+        <?php
+            require_once "../app/views/tamplates/admin-navbar.php";
+        ?>
+        <div class="content">
+            <div class="content-header">Transaction</div>
+            <div class="content-body">
+                <table class="table-transaction">
+                    <tr>
+                        <th>No</th>
+                        <th>No Payment</th>
+                        <th>No Booking</th>
+                        <th>Number Method Payment</th>
+                        <th>Method Payment</th>
+                        <th>Amount</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Status</th>
+                    </tr>
+                    
+                </table>
+            </div>
+        </div>
+    </div>
